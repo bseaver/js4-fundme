e1af283822d59474ca1c0b7185701e3490c23561
Benjamin T. Seaver and Stella Huayhuaca <stella@>
2017-04-06 13:31:55 -0700

    Add sign in link to Your Projects route

--- src/app/vip-detail/vip-detail.component.ts
+++ src/app/vip-detail/vip-detail.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { VipService } from './../vip.service';
+import { ProjectService } from './../project.service';
+import { Vip } from './../vip.model';
+import { Project } from './../project.model';
 
 @Component({
   selector: 'app-vip-detail',
   templateUrl: './vip-detail.component.html',
   styleUrls: ['./vip-detail.component.css'],
-  providers: [VipService]
+  providers: [VipService, ProjectService]
 })
 export class VipDetailComponent implements OnInit {
   username: string;
 
-  constructor(private vipService: VipService) { }
+  constructor(private vipService: VipService, private projectService: ProjectService) { }
 
   ngOnInit() {
     this.username = this.vipService.getUser();
